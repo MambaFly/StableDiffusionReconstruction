2b6b500b0b6e286d908c105af11ee66857a3487f
MambaFly <47297635+MambaFly@users.noreply.github.com>
2023-11-09 17:48:06 +0800

    Update make_subjmri.py
    fMRI数据预处理文件 添加注释

--- codes/utils/make_subjmri.py
+++ codes/utils/make_subjmri.py
@@ -4,8 +4,11 @@ import numpy as np
 import pandas as pd
 from nsd_access import NSDAccess
 import scipy.io
-
+'''
+fMRI数据预处理
+'''
 def main():
+    # 设置参数
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--subject",
@@ -17,7 +20,8 @@ def main():
     opt = parser.parse_args()
     subject = opt.subject
     atlasname = 'streams'
-    
+
+    # 调用nsd数据获取函数
     nsda = NSDAccess('../../nsd/')
     nsd_expdesign = scipy.io.loadmat('../../nsd/nsddata/experiments/nsd/nsd_expdesign.mat')
 
@@ -25,6 +29,7 @@ def main():
     # This is why subtracting 1
     sharedix = nsd_expdesign['sharedix'] -1 
 
+    # read_behavior 读取行为数据
     behs = pd.DataFrame()
     for i in range(1,38):
         beh = nsda.read_behavior(subject=subject, 
@@ -35,13 +40,16 @@ def main():
     stims_unique = behs['73KID'].unique() - 1
     stims_all = behs['73KID'] - 1
 
+    # 文件路径
     savedir = f'../../mrifeat/{subject}/'
     os.makedirs(savedir, exist_ok=True)
 
+    # 保存所有刺激和特有刺激
     if not os.path.exists(f'{savedir}/{subject}_stims.npy'):
         np.save(f'{savedir}/{subject}_stims.npy',stims_all)
         np.save(f'{savedir}/{subject}_stims_ave.npy',stims_unique)
 
+    # read_betas 读取betas文件
     for i in range(1,38):
         print(i)
         beta_trial = nsda.read_betas(subject=subject, 
@@ -53,7 +61,8 @@ def main():
             betas_all = beta_trial
         else:
             betas_all = np.concatenate((betas_all,beta_trial),0)    
-    
+
+    # read_atlas_results 读取Altas结果
     atlas = nsda.read_atlas_results(subject=subject, atlas=atlasname, data_format='func1pt8mm')
     for roi,val in atlas[1].items():
         print(roi,val)
@@ -66,6 +75,7 @@ def main():
         print(betas_roi.shape)
         
         # Averaging for each stimulus
+        # 对每个刺激取平均
         betas_roi_ave = []
         for stim in stims_unique:
             stim_mean = np.mean(betas_roi[stims_all == stim,:],axis=0)
@@ -87,7 +97,8 @@ def main():
         betas_tr = np.stack(betas_tr)
         betas_te = np.stack(betas_te)    
         
-        # AVERAGED DATA        
+        # AVERAGED DATA    
+        # 取均值
         betas_ave_tr = []
         betas_ave_te = []
         for idx,stim in enumerate(stims_unique):
@@ -99,6 +110,7 @@ def main():
         betas_ave_te = np.stack(betas_ave_te)    
         
         # Save
+        # 保存文件
         np.save(f'{savedir}/{subject}_{roi}_betas_tr.npy',betas_tr)
         np.save(f'{savedir}/{subject}_{roi}_betas_te.npy',betas_te)
         np.save(f'{savedir}/{subject}_{roi}_betas_ave_tr.npy',betas_ave_tr)
